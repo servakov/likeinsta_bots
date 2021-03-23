db4de31eaab1612e40f9c84287cc425a900026ec
Николай Серваков <41160218+servakov@users.noreply.github.com>
2021-03-23 19:11:45 +0300

    up

--- likeinsta_instagram_subscribe.py
+++ likeinsta_instagram_subscribe.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import re
 import time
 import json
 import sys
@@ -11,14 +10,15 @@ from pprint import pprint
 
 import urllib
 from time import sleep
+from bs4 import BeautifulSoup
 from chromote import Chromote
 
 
 def write_html (data, name = "log"):
     f = open (name+".htm","w")
+    #print data
     f.write (data)
     f.close
-
 
 def mt_rand (low = 0, high = sys.maxint):
     """Generate a better random value
@@ -26,14 +26,11 @@ def mt_rand (low = 0, high = sys.maxint):
     return random.randint (low, high)
 
 
-
 chrome = Chromote()
 tab = chrome.tabs[0]
-tab.set_url('https://likeinsta.ru/tasks/instagram/like/')
-#time.sleep(mt_rand(1,2))
-time.sleep(.900) # Wait for 900 milliseconds
-time.sleep(.900)
-for dotask in range(5):
+tab.set_url('https://likeinsta.ru/tasks/instagram/subscribe/')
+time.sleep(.990) # Wait 
+for dotask in range(11):
     print 'dotask: ' +  str(dotask)
     r = tab.evaluate('document.querySelectorAll("a.do-task")[' + str(dotask) + '].click()')
     print r
@@ -41,28 +38,26 @@ for dotask in range(5):
     """
     POP-UP
     """
-    time.sleep(mt_rand(8,10))
+    time.sleep(mt_rand(14,16))
     tab_popup = chrome.tabs[0]
-    print 'chrome POP-UP: '
-    print len(chrome)
     if len(chrome) == 1:
         continue
     #write_html(tab_popup.html)
+    soup = BeautifulSoup(tab_popup.html, 'html5lib')
 
     try:
-        r = tab_popup.evaluate('document.querySelectorAll("button[class*=wpO6b]")[1].click()')
+        tab_popup.evaluate("var element = document.querySelector('div.nZSzR button._5f5mN.jIbKX._6VtSN'); var o = document.createEvent('MouseEvents');o.initEvent('mousedown', true, true); element.dispatchEvent(o);")
+        r = tab_popup.evaluate('document.querySelector("button._5f5mN.jIbKX._6VtSN").click()')
         print r
-        
     except AttributeError:
         try:
             print 'except0'
-            r= tab_popup.evaluate('document.querySelector("a.fr66n span[class=ptsdu]").click()')
-            print r
         except AttributeError:
             print 'except1'
 
-    time.sleep(mt_rand(6,8))
+    time.sleep(mt_rand(3,5))
+    time.sleep(.300) # Wait for 300 milliseconds
     chrome.close_tab(tab_popup)
-    time.sleep(mt_rand(4,5))
+    time.sleep(mt_rand(3,5))
     r = tab.evaluate('document.querySelector("a.do.btn").click()')
     time.sleep(mt_rand(2,3))
