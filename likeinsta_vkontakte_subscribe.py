d2b28ffe9b4bec795d3151bffb4207abb65cfb14
Николай Серваков <41160218+servakov@users.noreply.github.com>
2021-03-23 18:44:34 +0300

    Update likeinsta_vkontakte_subscribe.py

--- likeinsta_vkontakte_subscribe.py
+++ likeinsta_vkontakte_subscribe.py
@@ -33,7 +33,7 @@ def mt_rand (low = 0, high = sys.maxint):
 chrome = Chromote()
 tab = chrome.tabs[0]
 tab.set_url('https://likeinsta.ru/tasks/vkontakte/subscribe/')
-time.sleep(.300) # Wait for 300 milliseconds
+time.sleep(.900) # Wait for 900 milliseconds
 
 for dotask in range(3):
     print 'dotask: ' +  str(dotask)
@@ -56,27 +56,36 @@ for dotask in range(3):
 
     try:
         print 'Groups.enter:'
-        btn3 = soup.find('button', {'id': 'join_button'}).get('onclick')
+        #btn3 = soup.find('button', {'id': 'join_button'}).get('onclick')
         #print btn3
-        r = tab_popup.evaluate(btn3)
+        #r = tab_popup.evaluate(btn3)
+        r = tab_popup.evaluate('document.querySelectorAll("button#join_button")[0].click()')
         print r
+        r = tab_popup.evaluate('document.querySelector("#page_actions button#public_subscribe").click()')
+        print r
+        r = tab_popup.evaluate('document.getElementsByClassName("profile_action_btn")[1].getElementsByTagName("button")[0].click()')
+        print r
+
     except AttributeError:
         print 'Profile.toggleFriend:'
         r = tab_popup.evaluate('document.getElementsByClassName("profile_action_btn")[1].getElementsByTagName("button")[0].click()')
         print r
-        try:
 
+        try:
             print 'acceptFriendBtn!'
             r = tab_popup.evaluate('document.querySelectorAll("a.acceptFriendBtn")[0].click()')
             print r
 
         except AttributeError:
             print 'AttributeError=>Public.subscribe:'
-            r = tab_popup.evaluate('Public.subscribe()')
+            #r = tab_popup.evaluate('Public.subscribe()')
+            r = tab_popup.evaluate('document.querySelector("#page_actions button#public_subscribe").click()')
             print r
         else: 
-            print 'else: Public.subscribe:'
-            r = tab_popup.evaluate('Public.subscribe()')
+            print 'else: Group.subscribe:'
+            r = tab_popup.evaluate('document.querySelectorAll("button#join_button")[0].click()')
+            print r
+
 
     time.sleep(.990) # Wait for 990 milliseconds
     time.sleep(mt_rand(6,7))
